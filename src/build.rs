173068c5c86c83939b4afd8722e37b37f231535a
Snehit Sah <snehitsah@protonmail.com>
2021-12-25 17:23:14 +0530

    Gallery generation works
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- src/build.rs
+++ src/build.rs
@@ -1,11 +1,18 @@
 use handlebars::Handlebars;
+use serde::{Deserialize, Serialize};
+use serde_json::Value as Json;
 use std::{collections::BTreeMap, fs, path::PathBuf};
 
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Language {
     title: String,
+    img_url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Picture {
+    title: String,
+    img_small_url: String,
     img_url: String,
 }
 
@@ -39,6 +46,11 @@ pub fn build(template_path: PathBuf) {
         p.push("index.hbs".to_string());
         p
     };
+    let gallery_template_path = {
+        let mut p = template_path.clone();
+        p.push("gallery.hbs".to_string());
+        p
+    };
     let base_template_path = {
         let mut p = template_path.clone();
         p.push("base.hbs".to_string());
@@ -51,20 +63,91 @@ pub fn build(template_path: PathBuf) {
         .register_template_string("base", hbs_base_template)
         .unwrap();
 
-    // let mut handlebars = Handlebars::new();
-    render_index(index_template_path, languages, &handlebars);
+    render_index(index_template_path, &languages, &handlebars);
+    render_gallery(gallery_template_path, &languages, &handlebars);
 }
 
-fn render_index(index_template_path: PathBuf, languages: Vec<Language>, handlebars_g: &Handlebars) {
+fn render_gallery(
+    gallery_template_path: PathBuf,
+    languages: &Vec<Language>,
+    handlebars_g: &Handlebars,
+) {
+    let mut data: BTreeMap<String, Json> = BTreeMap::new();
+
+    // data.insert("language".to_string(), language);
+    data.insert("languages".to_string(), handlebars::to_json(&languages));
+
+    let handlebars = {
+        let mut h = handlebars_g.clone();
+        let gallery_template = fs::read_to_string(&gallery_template_path).unwrap();
+        h.register_template_string("gallery", gallery_template)
+            .unwrap();
+        h
+    };
+
+    let mut output_path = PathBuf::new();
+    output_path.push(".");
+    output_path.push("www");
+
+    let img_url_base: String =
+        "https://raw.githubusercontent.com/cat-milk/Anime-Girls-Holding-Programming-Books/master/"
+            .to_string();
+
+    for language in languages {
+        let mut pictures: Vec<Picture> = vec![];
+
+        let entries = fs::read_dir(format!("{}/{}", ".", language.title.clone()))
+            .expect("Can't read current directory");
+
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_file() {
+                    pictures.push(Picture {
+                        title: path.to_str().unwrap().to_string(),
+                        img_url: format!(
+                            "{}{}/{}",
+                            img_url_base,
+                            &language.title,
+                            path.clone().file_name().unwrap().to_str().unwrap()
+                        ),
+                        img_small_url: format!(
+                            "{}{}/{}",
+                            img_url_base,
+                            &language.title,
+                            path.clone().file_name().unwrap().to_str().unwrap()
+                        ),
+                    });
+                }
+            }
+        }
+
+        data.insert(
+            "language".to_string(),
+            handlebars::to_json(language.title.clone()),
+        );
+        data.insert("pictures".to_string(), handlebars::to_json(&pictures));
+        output_path.push(&language.title);
+        output_path.set_extension("html");
+
+        fs::write(
+            &output_path,
+            handlebars.render("gallery", &data).unwrap().to_string(),
+        )
+        .unwrap();
+        output_path.pop();
+    }
+}
+
+fn render_index(
+    index_template_path: PathBuf,
+    languages: &Vec<Language>,
+    handlebars_g: &Handlebars,
+) {
     let mut data: BTreeMap<String, Vec<Language>> = BTreeMap::new();
-    data.insert("languages".to_string(), languages);
+    data.insert("languages".to_string(), languages.clone().to_vec());
 
     let hbs_index_template = fs::read_to_string(&index_template_path).unwrap();
-
-    println!(
-        "start read string \n{}\nend read string",
-        hbs_index_template
-    );
 
     let mut handlebars = handlebars_g.clone();
 
